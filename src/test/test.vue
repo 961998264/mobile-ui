a911742b2fc3189b019e661730103ae261d2228d
yongming.lin@yintech.cn <yongming.lin@yintech.cn>
2021-05-18 14:32:18 +0800

    add

---
+++ src/test/test.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class='main-wrap'>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+  },
+  components: {
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+    //api 
+
+
+  },
+  computed: {
+
+  },
+  watch: {
+
+  },
+  created () {
+  },
+  mounted () { },
+}
+</script>
+
+<style lang='scss' scoped>
+.main-wrap {
+}
+</style>
